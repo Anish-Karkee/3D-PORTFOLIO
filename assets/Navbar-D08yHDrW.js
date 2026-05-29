7839d66efa077cf5b3c3266514750c95192384e0
Anish-Karkee <anitakarkey7@gmail.com>
2026-05-29 07:34:34 +0545

    Updates

--- assets/Navbar-D08yHDrW.js
+++ assets/Navbar-D08yHDrW.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-BUbANoY2.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-yr51SDM3.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
